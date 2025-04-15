4e9a6e59d7ebf0207feb95cea544dec8e063c6d2
na diagonal <nadiagonal@proton.me>
2025-04-15 22:07:10 +0100

    Pasta para os testes e tarefas para verificador adicionadas

---
+++ MarcaAguaTexto/tests/test_controller/test_controller_methods.py
@@ -0,0 +1,27 @@
+''' TODO Verificador
+test_controller_methods.py
+Ficheiro que contém testes para verificar o
+comportamento dos métodos do componente controller'''
+
+import pytest
+
+from unittest.mock import MagicMock
+
+import MarcaAguaTexto.Controller.controller
+import MarcaAguaTexto.Controller.wmk_controller_encoding
+
+# Teste para a conversão do número do destinário no código binário
+def test_binary_converter():
+    pass
+
+# Teste para gerar o código CRC-8 a partir de uma string binária
+def test_error_detection():
+    pass
+
+# Teste para a subsituir os espaços por texto por espaços ou espaços ininterruptos
+def test_encode_binary_into_text():
+    pass
+
+# Teste para a codificação do texto
+def test_codificar_texto():
+    pass
---
+++ MarcaAguaTexto/tests/test_main.py
@@ -0,0 +1,19 @@
+''' TODO Verificador
+test_main.py
+Ficheiro que contém testes para verificar o
+comportamento de iniciar_programa e da inicialização
+dos componentes
+controller, model e view
+em MarcaAguaTexto.py'''
+
+import pytest
+
+from unittest.mock import MagicMock
+import MarcaAguaTexto.Model.model
+import MarcaAguaTexto.Controller.controller
+import MarcaAguaTexto.View.view
+import MarcaAguaTexto.MarcaAguaTexto
+
+# Teste para inciar programa
+def test_main():
+    pass
